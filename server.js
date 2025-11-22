06d2b8926add0179513da2961b2048d1793a755f
snpandeylicidgp-tech <snpandeylicidgp@gmail.com>
2025-11-22 20:33:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const cors=require('cors');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
+const app=express();
+app.use(cors());
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URI).then(()=>console.log('Mongo Connected'));
+app.get('/',(req,res)=>res.send('Smart HomeCare Backend Running'));
+app.listen(process.env.PORT||5000);
